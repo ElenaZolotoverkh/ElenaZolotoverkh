b43841bdf1e8170547f792cb740e03a3120f01d2
Elena.Zolotoverkh <Elena.Zolotoverkh@devellar.com>
2015-08-04 18:15:37 +0300

    lesson5

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+    public static int f() {
+        return 1000;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(k);
+        int k =  2;
+
+        // write your code here
+        //   int i = 1000;
+        //    System.out.println(i);
+        //    int x = f();
+        //    System.out.println(x);
+       // int[] ns = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //int t = 1;
+        //int x = m(ns, t);
+    }
+
+   /* public static int m(int[] ns, int t) {
+        int result = 0;
+        for (int i = 0; i < 11; i++)
+            if (ns[i] == t) {
+                result++;
+            }
+
+
+        return result;
+    }*/
+
+
+    public static int umn(){
+
+        int k =  2;
+        return k;
+
+    }
+
+}
