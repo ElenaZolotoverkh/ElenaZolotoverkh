c49b7cae0b76d7077da01b3de9303bfb373167ca
ElenaZolotoverkh <Zolotoverkh_Olena@ukr.net>
2015-08-04 16:14:03 +0300

    First 3 lessons

---
+++ HW3_1/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 20;
+        int b = a + 5;
+        String s = "Test String";
+        while (a > 0) {
+            System.out.println("Recycle " + b);
+            a = a - 5;
+        }
+        System.out.println();
+        for (int u = 2000; u > 918; u = 918) {
+            System.out.print("continue");
+        }
+
+
+
+    }
+}
---
+++ QA3/QA3/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a = 10;
+        if (a < 10){
+            System.out.println("a<10");
+        }
+        else {System.out.println("a>=10");}
+    }
+    {
+    int xy = 0;
+    while (xy < 10){
+        xy = xy + 5;
+        System.out.println(xy);
+
+        }}
+
+
+}
+
+
