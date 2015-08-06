323c7c3a810bffc55cc1b2459c3ee3bdeaf6afcd
Elena Zolotoverkh <ElenaZolotoverkh@gmail.com>
2015-08-06 21:06:51 +0300

    lesson7

---
+++ QA3/src/com/company/Lesson7.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * Created by admin on 06.08.15.
+ */
+public class Lesson7 {
+    public static void main(String[] args){
+        System.out.println(a);
+        int a=20;
+        func();
+        ////////
+        int[] b = {1,2,-20,20};
+        for(int i : b){  //new method for
+            System.out.println(i);
+    }
+        //////////
+
+    }
+    public static void func(){
+        System.out.println(a);
+    }
+    public static int a= 10;
+    public static void func2(){
+        int a = 30;
+        System.out.println(a);
+        func();
+    }
+    }
+
