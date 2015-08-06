40621d4abf112f6b5d4dd24819d4edb2b5617063
Elena Zolotoverkh <ElenaZolotoverkh@gmail.com>
2015-08-06 21:06:22 +0300

    write and find info in google

--- QA3/src/com/company/Silenium1_Les6.java
+++ QA3/src/com/company/Silenium1_Les6.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 /**
  * Created by admin on 04.08.15.
@@ -9,9 +11,23 @@ public class Silenium1_Les6 {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
         ChromeDriver driver= new ChromeDriver();
-        driver.get("https://www.google.ru");//oткрыть сайт
-        Thread.sleep(5000);// подождать 5 сек
-        driver.quit();//закрыть браузер
+        driver.get("https://www.google.com.ua");//open site
+
+        System.out.println(driver.getTitle()); //take title
+        driver.findElementByName("q");
+        WebElement j = driver.findElementByName("q");
+        j.sendKeys("cats");
+        j.sendKeys(Keys.ENTER);
+        driver.findElementByName("btnG").click();
+        /* vait 5 second */
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        driver.quit();//Р·Р°РєСЂС‹С‚СЊ Р±СЂР°СѓР·РµСЂ
+
     }
 
 }
+
