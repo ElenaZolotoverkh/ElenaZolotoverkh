411fa787137f9bde941eeae04d8bb7e0833acd8e
Elena Zolotoverkh <ElenaZolotoverkh@gmail.com>
2015-08-11 19:28:57 +0300

    HW_LEs7

--- QA3/src/com/company/Lesson6_HW.java
+++ QA3/src/com/company/Lesson6_HW.java
@@ -13,6 +13,7 @@ public class Lesson6_HW {
         s.add("b");
         s.add("c");
         s.add("d");
+        s.add("b");
         print(s);
 
         ArrayList<String> f = new ArrayList<String>();
@@ -20,22 +21,40 @@ public class Lesson6_HW {
         f.add("b");
         f.add("c");
         f.add("d");
-        //  f.add("d");
+        f.add("d");
+        f.add("d");
 
         System.out.println(isListsEqual(s, f));
-    }
-    public static void print(ArrayList s){
 
-        System.out.println(s);
-    }
+        ArrayList<String> s1 = new ArrayList<String>();
+        ArrayList<String> s2 = new ArrayList<String>();
+        System.out.println(isListsEqual(s1, s2));
 
-    public static boolean isListsEqual(ArrayList s, ArrayList f ){
-        if (s.equals(f)){
-            return true;
-        } else
-            return false;
+        String[] a = {"a", "b", "c", "d", "b"};
+        print(a);
 
     }
+        public static void print(ArrayList s){
+
+            System.out.println(s);
+        }
+
+        public static boolean isListsEqual(ArrayList s, ArrayList f ){
+            if (s.equals(f)){
+                return true;
+            } else
+                return false;
+
+        }
+    public static void print(String[] a) {
+        // Тут дописать тело функции.
+
+       for  ( int i = 0; i<5; i += 1){
+        System.out.print(a[i]);
+        }
+
+    }
+
 
 
 }
