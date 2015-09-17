b603a4f2eaec7a84eed94b6df299d02f380dedf0
sherilwolf <sherilwolf@gmail.com>
2015-09-17 21:02:27 +0300

    upload files

---
+++ QA3/src/com/company/UploadFile.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by Admin on 17.09.15.
+ */
+public class UploadFile {
+/**
+    public void robot(){
+
+    Robot robot = new Robot();
+    robot.keyPress(KeyEvent.VK_CONTROL);
+    robot.keyPress(KeyEvent.VK_V);
+    robot.keyRelease(KeyEvent.VK_V);
+    robot.keyRelease(KeyEvent.VK_CONTROL);
+    robot.keyPress(KeyEvent.VK_ENTER);
+    robot.keyRelease(KeyEvent.VK_ENTER);
+}
+ */
+}
