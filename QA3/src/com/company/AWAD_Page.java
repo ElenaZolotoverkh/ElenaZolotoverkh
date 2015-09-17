43022a8f3699bbb313c1d671cfed346f83905f91
sherilwolf <sherilwolf@gmail.com>
2015-09-17 20:13:05 +0300

    https://www.anywayanyday.com/

---
+++ QA3/src/com/company/AWAD_Page.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by Admin on 17.09.15.
+ */
+public class AWAD_Page {
+    public static String url = "https://www.anywayanyday.com/";
+
+}
---
+++ QA3/src/com/company/AWAD_Test.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * Created by Admin on 17.09.15.
+ */
+public class AWAD_Test {
+}
