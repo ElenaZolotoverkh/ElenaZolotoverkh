b68b1bd3d91814e84c336fd5db506f3eda50c2a6
sherilwolf <sherilwolf@gmail.com>
2015-09-17 19:41:24 +0300

    fix enc

--- QA3/src/com/company/Lesson6.java
+++ QA3/src/com/company/Lesson6.java
@@ -9,14 +9,14 @@ import java.util.Arrays;
 public class Lesson6 {
     public static void main(String[] args){
         ArrayList<Integer> a = new ArrayList<Integer>();
-        System.out.println(a.size());//узнать длину списка
-        a.add(10);//добавить значение в список
+        System.out.println(a.size());//СѓР·РЅР°С‚СЊ РґР»РёРЅСѓ СЃРїРёСЃРєР°
+        a.add(10);//РґРѕР±Р°РІРёС‚СЊ Р·РЅР°С‡РµРЅРёРµ РІ СЃРїРёСЃРѕРє
         System.out.println(a.size());
         a.add(1);
         a.add(5);
-        System.out.println(a.get(1));//достать значение со списка
+        System.out.println(a.get(1));//РґРѕСЃС‚Р°С‚СЊ Р·РЅР°С‡РµРЅРёРµ СЃРѕ СЃРїРёСЃРєР°
         System.out.println(a.get(2));
-        a.remove(0);//удалить значение со списка
+        a.remove(0);//СѓРґР°Р»РёС‚СЊ Р·РЅР°С‡РµРЅРёРµ СЃРѕ СЃРїРёСЃРєР°
         System.out.println(a.size());
         Integer []a1 ={1, 2, 10, 20};
         a.addAll(Arrays.asList(a1));
